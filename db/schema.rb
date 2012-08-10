03d754bb7c0dbf37e795050da8095df4aecd8756
edwardchiu38 <edwardchiu38@gmail.com>
2012-08-10 13:49:05 -0700

    reconfigures model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120803223148) do
+ActiveRecord::Schema.define(:version => 20120810204817) do
 
   create_table "tractions", :force => true do |t|
     t.string   "name"
